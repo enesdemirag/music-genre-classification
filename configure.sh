0acca59e35b6f4c2867046d8b803eb5c1535f11b
Enes Demirag <ensdmrg@gmail.com>
2020-11-16 18:23:42 +0300

    Updated configuration file for linux

--- configure.sh
+++ configure.sh
@@ -4,3 +4,6 @@ pip3 install numpy matplotlib librosa
 # Dataset
 wget http://opihi.cs.uvic.ca/sound/genres.tar.gz
 tar -xzf genres.tar.gz
+mkdir -p dataset/audios
+mv genres/* -v dataset/audios/
+rm -rf genres genres.tar.gz
