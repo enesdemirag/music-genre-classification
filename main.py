8f0f7978b727e111fe68f0bbb530f21d1580af48
Enes Demirag <ensdmrg@gmail.com>
2020-11-16 18:13:29 +0300

    created structure for different models

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ img = matrix2image(spec)
 
 # Creating models
 mlp = MLP(spec.shape, len(classes), 0.01)
+# cnn = CNN(spec.shape, len(classes), 0.01)
+
+# Training
+# epochs, hist = train_model(mlp, train_data, labels, epochs=10)
+# plot_training(epochs, hist)
+
+# Testing
+# accuracy = test_model(mlp, test_data, labels)
+# print(result)
 
 # Prediction
 y = predict(mlp, img)
