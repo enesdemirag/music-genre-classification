b596e12f1f0c2405428baeb2382bc7fc9a1f7122
Enes Demirag <ensdmrg@gmail.com>
2020-11-16 21:22:38 +0300

    add spectrogram directory

--- configure.sh
+++ configure.sh
@@ -5,5 +5,6 @@ pip3 install numpy matplotlib librosa tensorflow pandas pillow
 wget http://opihi.cs.uvic.ca/sound/genres.tar.gz
 tar -xzf genres.tar.gz
 mkdir -p dataset/audios
+mkdir dataset/spectrograms
 mv genres/* -v dataset/audios/
 rm -rf genres genres.tar.gz
