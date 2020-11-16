b95ae5f54990247a59779bf246034af4d2bca7b1
Enes Demirag <ensdmrg@gmail.com>
2020-11-16 18:12:51 +0300

    Simple workflow

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from audio2spectrogram import *
+from train import * 
+from test import *
+from models import *
+from preprocessing import *
 
+# Preprocessing
 spec, fs = get_spectrogram('dataset/audios/blues.wav')
-save_spectrogram(spec, 'dataset/spectrograms/blues', 'image')
+img = matrix2image(spec)
 
+# Creating models
+mlp = MLP(spec.shape, len(classes), 0.01)
+
+# Prediction
+y = predict(mlp, img)
+print(y)
